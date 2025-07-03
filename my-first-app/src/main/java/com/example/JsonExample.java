a8bee5c563512b74026dd0a2756d8102b8bb67fd
Dana-Klesly <dndonklesly@gmail.com>
2025-07-03 16:08:42 +0200

    Add files via upload

---
+++ my-first-app/src/main/java/com/example/JsonExample.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import com.google.gson.Gson;
+
+class Person {
+    String name;
+    int age;
+
+    Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+public class JsonExample {
+    public static void main(String[] args) {
+        Gson gson = new Gson();
+        Person person = new Person("Alice", 30);
+        String json = gson.toJson(person);
+        System.out.println("JSON Output: " + json);
+    }
+}
